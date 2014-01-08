33ab6b39a16497ac066abb8f8e88d65ae530298c
kernkw <kyle.w.kern@gmail.com>
2014-01-08 14:30:11 -0700

    Add precompiled assets for Heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 TutorialApp::Application.config.secret_key_base = '2ba238eedda55934a0ed3099cb346cd446375e370943370a783454d439f1bec2139efa8c15d40fc8e1f35928c67bdd175b7753f531e19e1dd121a93f9ec0c5b7'
